9a4b07be6b16e2ace84c398c44da2e1098893d9d
mkulumadzi <evan.waters@gmail.com>
2015-03-11 12:52:44 -0700

    Initial Commit

---
+++ SnailMail/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnailMail
+//
+//  Created by Evan Waters on 3/11/15.
+//  Copyright (c) 2015 Evan Waters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
